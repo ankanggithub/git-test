7301c438741015752a182af5d6decd609d2e8093
ankang <ankang95101@qq.com>
2019-05-08 20:46:23 +0800

    把本地git-test工程添加到Git

---
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+/**
+ * @Package: git-test com.itheima.git
+ * @Author: AK
+ * @Date: 2019/5/8 20:29
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project！");
+        System.out.println("Hello Git Project！");
+        System.out.println("Hello Git Project！");
+        System.out.println("Hello Git Project！");
+        System.out.println("Hello Git Project！");
+        System.out.println("Hello Git Project！");
+        System.out.println("Hello Git Project！");
+    }
+}
